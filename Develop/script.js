3c25f3715e396deb9eb9336b12b42b66589cc23a
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-03-28 19:01:08 -0700

    added object for password generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,50 @@
 // Assignment code here
+var generatepassword = function() {
+  var lengthprompt = window.prompt("how long would you like your password to be? \n Min: 8 \n Max: 128");
+  lengthprompt = parseInt(lengthprompt)
+  if (lengthprompt <= 7 || lengthprompt >= 129 || lengthprompt === null || lengthprompt === "") {
+    window.alert("please select proper passsword length");
+    return generatepassword();
+  }
 
+  var lowerCaseConfirm = window.confirm("Would you like your password to include lower case letters?");
+  if (lowerCaseConfirm) {
+    
+  } else {
+    charArray.lower = "";
+  }
+  var upperCaseConfirm = window.confrim("Would you like your password to include upper case letters?");
+  if (upperCaseConfirm) {
+    
+  } else{
+    charArray.upper = "";
+  }
+  var numberConfirm = window.confirm("would you like your password to include numbers?");
+  if (numberConfirm) {
 
+  } else {
+    charArray.number = ""
+  }
+  var symbolConfirm = window.confirm("would you like your password to include symbols?");
+  if (symbolConfirm) {
+
+  }else {
+    charArray.symbol = ""
+  }
+
+}
+
+//variables for password.
+var charArray = {
+  lower: "abcdefghijklmnopqrstuvwxyz",
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  number: "1234567890",
+  symbol: "!#$%&*+,-./:;<=>?@[]^_{|}~",
+  combine: function() {
+    array = this.lower.concat(this.upper, this.number, this.symbol);
+    return array;
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +59,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
