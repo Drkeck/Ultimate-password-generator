440697eef34b88a62b12f3b5a6bcd6b6d379998b
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-03-28 21:24:25 -0700

    fixed bug and refined the code

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,27 @@
 // Assignment code here
 var generatePassword = function() {
   var lengthprompt = window.prompt("how long would you like your password to be? \n Min: 8 \n Max: 128");
-    if (lengthprompt === "" || lengthprompt === null) {
-      window.alert("please input a number");
-      generatePassword();
-    }
-  lengthprompt = parseInt(lengthprompt);
-  if (lengthprompt <= 7 || lengthprompt >= 129) {
-    window.alert("please select proper passsword length");
-    generatePassword();
-  }
-  // confirm lower case
-  charArray.confirmL();
+  debugger;
+    if (lengthprompt < 8 || lengthprompt > 128 || isNaN(lengthprompt) == true) {
+      window.alert("please select proper passsword length");
+    } else {
+      //ask what elements they want then combine.
+      charArray.combine();
 
-  //confrim upper case
-  charArray.confrimU();
-
-  //confrim numbers
-  charArray.confirmN();
-
-  //confirm symbols
-  charArray.confirmS();
-
-  //combine all arrays
-  charArray.combine();
-
-  //if line is empty
-  if (charArray.array === "") {
-    alert("Please select at least one thing to include in your randomly generated password. \n Click the button to try again.");
-    generatePassword();
-  } 
-  else {
-    //make a password from array
-    var displayPass = '';
-    for(var i = 0; i < lengthprompt; i++) {
-    displayPass += charArray.array[Math.floor(Math.random() * charArray.array.length)];
-    }
-    return displayPass;
-  }
+      //if line is empty
+      if (charArray.array === "") {
+        alert("Please select at least one thing to include in your randomly generated password. \n Click the button to try again.");
+      } 
+      else {
+        //make a password from array
+        var displayPass = '';
+        for(var i = 0; i < lengthprompt; i++) {
+        displayPass += charArray.array[Math.floor(Math.random() * charArray.array.length)];
+        }
+        return displayPass;
+      };
+    };
+  return "Try Again"
 }
 
 //variables for password.
@@ -48,39 +32,31 @@ var charArray = {
   symbol: "!#$%&*+,-./:;<=>?@[]^_{|}~",
   array: "",
   combine: function() {
-    this.array = this.lower.concat(this.upper, this.number, this.symbol);
-  },
-  confirmL: function() {
-      var lowerCaseConfirm = confirm("Would you like your password to include lower case letters?");
-      if (lowerCaseConfirm) {
-         this.lower = 'abcdefghijklmnopqrstuvwxyz'
-      } else {
-          this.lower = "";
-      }
-  },
-  confrimU: function() {
-      var upperCaseConfirm = confirm("Would you like your password to include upper case letters?");
-      if (upperCaseConfirm) {
-          this.upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-      } else {
-          this.upper = "";
-      }
-  },
-  confirmN: function() {
-      var numberConfirm = confirm("Would you like numbers included in your password?");
-      if (numberConfirm) {
-        this.number = "1234567890";
-      } else {
-        this.number = "";
-      }
-  },
-  confirmS: function() {
+    var lowerCaseConfirm = confirm("Would you like your password to include lower case letters?");
+    if (lowerCaseConfirm) {
+        this.lower = 'abcdefghijklmnopqrstuvwxyz'
+    } else {
+        this.lower = "";
+    }
+    var upperCaseConfirm = confirm("Would you like your password to include upper case letters?");
+    if (upperCaseConfirm) {
+         this.upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    } else {
+        this.upper = "";
+    }
+    var numberConfirm = confirm("Would you like numbers included in your password?");
+    if (numberConfirm) {
+      this.number = "1234567890";
+    } else {
+      this.number = "";
+    }
     var symbolconfirm = confirm("Would you like symbols to be included in your password?");
     if (symbolconfirm) {
       this.symbol = "!#$%&*+,-./:;<=>?@[]^_{|}~"
     } else {
       this.symbol = ""
     }
+    this.array = this.lower.concat(this.upper, this.number, this.symbol);
   }
 }
 // Get references to the #generate element
